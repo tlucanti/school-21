8f2d51605110c757b42dba241b02a24b4d284057
Tillie Lucantine <tlucanti@am-d2.msk.21-school.ru>
2021-01-20 12:07:43 +0300

    GNL-02

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tlucanti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/20 12:00:38 by tlucanti          #+#    #+#             */
-/*   Updated: 2021/01/20 12:00:43 by tlucanti         ###   ########.fr       */
+/*   Created: 2021/01/20 12:00:54 by tlucanti          #+#    #+#             */
+/*   Updated: 2021/01/20 12:00:58 by tlucanti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCHOOL21_GET_NEXT_LINE_H
-# define SCHOOL21_GET_NEXT_LINE_H
+#ifndef SCHOOL21_GET_NEXT_LINE_BONUS_H
+# define SCHOOL21_GET_NEXT_LINE_BONUS_H
 
 # include <stddef.h>
 # include <stdlib.h>
