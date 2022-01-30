8389e64f70ddf9d1bc36dea9b8e68c999ad70414
tlucanti <prototype3628800@yandex.ru>
2022-01-30 18:17:32 +0300

    CPP-07/ex02 macro fix, CPP-08/ex00 more tests

--- CPP-08/ex00/main.cpp
+++ CPP-08/ex00/main.cpp
@@ -16,24 +16,58 @@
 #include <vector>
 #include <list>
 #include <set>
+#include <iomanip>
 
 template <typename type_T>
 type_T generate(int i)
 {
 	type_T a;
 	for (int j=0; j < i; ++j)
-		a.emplace_back(j);
+		a.push_back(j);
+	return a;
+}
+
+template <typename type_T>
+type_T insert(int i)
+{
+	type_T a;
+	for (int j=0; j < i; ++j)
+		a.insert(j);
 	return a;
 }
 
 int main()
 {
+	typedef	std::vector<int>	V;
+	typedef std::set<int> S;
+	typedef std::set<int> M;
 	{
-		typedef	std::vector<int>	T;
-		T a = generate<T>(4);
+		V a = generate<V>(4);
 		std::cout << *tlucanti::easyfind(a, 3) << std::endl;
 	}
 	{
+		V a = generate<V>(3);
+		V::const_iterator it = tlucanti::easyfind(a, -1);
+		std::cout << std::boolalpha << (it == a.cend()) << std::endl;
+	}
 
+	{
+		S a = insert<S>(4);
+		std::cout << *tlucanti::easyfind(a, 3) << std::endl;
+	}
+	{
+		S a = insert<S>(3);
+		S::const_iterator it = tlucanti::easyfind(a, -1);
+		std::cout << std::boolalpha << (it == a.cend()) << std::endl;
+	}
+
+	{
+		M a = insert<M>(4);
+		std::cout << *tlucanti::easyfind(a, 3) << std::endl;
+	}
+	{
+		M a = insert<M>(3);
+		M::const_iterator it = tlucanti::easyfind(a, -1);
+		std::cout << std::boolalpha << (it == a.cend()) << std::endl;
 	}
 }
