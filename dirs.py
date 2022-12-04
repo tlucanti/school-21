5e12dac22c2c7146723b7c9e96b4c6804f04d8b9
tlucanti <prototype3628800@yandex.ru>
2022-12-04 13:30:56 +0300

    add tree structure

--- dirs.py
+++ dirs.py
@@ -5,6 +5,7 @@ for i in range(14):
     code = input()
     os.system(f'mkdir -p level{i:02}/resources')
     os.system(f'echo {code} > level{i:02}/flag')
+    os.system(f'touch level{i:02}/resources/.gitkeep')
 
 os.system('cp gdb.txt level00/resources')
 
--- filter.sh
+++ filter.sh
@@ -1,1 +1,3 @@
 cat out.txt | grep '(gdb) (gdb)' | cut -c 13- > flags.txt
+python3 dirs.py < flags.txt
+
