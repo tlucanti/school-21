682dba48360a2208fcecce20fa8141a5b1920bfa
tlucanti <prototype3628800@yandex.ru>
2022-12-04 13:27:43 +0300

    initial commit

---
+++ dirs.py
@@ -0,0 +1,10 @@
+
+import os
+
+for i in range(14):
+    code = input()
+    os.system(f'mkdir -p level{i:02}/resources')
+    os.system(f'echo {code} > level{i:02}/flag')
+
+os.system('cp gdb.txt level00/resources')
+
---
+++ filter.sh
@@ -0,0 +1,1 @@
+cat out.txt | grep '(gdb) (gdb)' | cut -c 13- > flags.txt
